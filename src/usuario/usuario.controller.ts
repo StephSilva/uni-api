b63b0c58ede824f7eb46d8eb4735c28c5fa1f0c1
StephSilva <52845817+StephSilva@users.noreply.github.com>
2022-04-30 09:30:51 -0600

    agregado busqueda por texto a la tabla usuario

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Res } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { Request, Response } from 'express';
 
 @Controller('usuario')
 export class UsuarioController {
@@ -13,8 +14,14 @@ export class UsuarioController {
   }
 
   @Get()
-  findAll() {
-    return this.usuarioService.findAll();
+  async findAll(@Req() request: Request, @Res() response: Response) {
+    try {
+      // Le pasamos el texto a buscar que viene dentro del objeto request.query
+      const data = await this.usuarioService.findAll(request.query.nombre as string);
+      response.status(200).send(data);
+    } catch (error) {
+      response.status(400).send("Error al encontrar usuarios");
+    }
   }
 
   @Get(':id')
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateLoginDto } from 'src/login/dto/create-login.dto';
-import { Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { Usuario } from './entities/usuario.entity';
@@ -18,8 +18,17 @@ export class UsuarioService {
     return this.repositorio.save(createUsuarioDto);
   }
 
-  findAll() {
-    return this.repositorio.find({ relations: ['rol'] });
+  findAll(nombre = "") {
+    // por defecto tenemos el nombre en vacio,
+    let filter = {};
+    //aplicamos el filtro si la variable nombre contiene algun texto a buscar
+    if(nombre.length){
+      //Buscamos por ilike para ignorar mayusculas o minusculas
+      // usamos el operador % al inicio y al final para buscar en la coincidencia en cualquier parte de la palabra
+      filter = { nombre: ILike(`%${nombre}%`) }
+    }
+    //le pasamos el objeto filter para establecer la condicion
+    return this.repositorio.find({ relations: ['rol'], where: { ...filter } });
   }
 
   buscarPorNombreUsuario(params: CreateLoginDto) {
